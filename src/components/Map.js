aeb7bbb055b868a86e402ffaab5fb73825c8ce37
Lisa Sundberg <lisa.sundberg@hyperisland.se>
2018-06-11 14:19:26 +0200

    Create object for locations and write out in components using map

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Pin from './Pin';
 
-
 class Map extends Component {
   static defaultProps = {
     center: {
       lat: 59.31,
       lng: 18.07
     },
-    zoom: 12
+    zoom: 12,
+    greatPlaces: [
+      {id: 'Vitabergsparken', lat: 59.31255, lng: 18.087676},
+      {id: 'Tantolunden', lat: 59.313137, lng: 18.037121}
+    ]
   };
 
   render() {
+    const places = this.props.greatPlaces
+      .map(place => {
+        const { id, lat, lng } = place;
+        return (
+          <Pin
+            key={id}
+            lat={lat}
+            lng={lng}
+            title={id}
+          />
+        );
+      });
 
     return (
       // Important! Always set the container height explicitly
@@ -20,18 +35,8 @@ class Map extends Component {
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyCTpQfzQtswW-XYC8SiR_IHKVCvutdgF7g' }}
           defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <Pin
-            lat={59.312555}
-            lng={18.087676}
-            title="Vitabergsparken"
-          />
-          <Pin
-            lat={59.313137}
-            lng={18.037121}
-            title="Tantolunden"
-          />
+          defaultZoom={this.props.zoom}>
+          { places }
         </GoogleMapReact>
       </div>
     );
