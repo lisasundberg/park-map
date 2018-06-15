991ca5c31052e59bffe6756a9ff51689aae65675
Lisa Sundberg <lisa.sundberg@hyperisland.se>
2018-06-15 14:57:00 +0200

    Write out filters in header

--- src/components/FilterBox.js
+++ src/components/FilterBox.js
@@ -5,7 +5,7 @@ import FilterOption from './FilterOption';
 class FilterBox extends Component {
 
   state = {
-    choosenFilters: []
+    chosenFilters: []
   }
 
  closeFilterBox = () => {
@@ -13,78 +13,75 @@ class FilterBox extends Component {
   }
 
   getFilterValue = (lol) => {
-    let array = this.state.choosenFilters;
+    let array = this.state.chosenFilters;
     array.push(lol);
     this.setState({
-      choosenFilters: array
+      chosenFilters: array
     });
   }
 
 render(){
   console.log(document.getElementById('filterOutputBox'))
   return (
+    <div>
+      <div className="chosenFilters">
+        { this.state.chosenFilters.map(function(filter, index){
+          return <p key={index} className="filterItem"> {filter} </p>;
+        })}
+      </div>
     <div className={`filterBox ${this.props.className}`}>
       <div>
-      <div className="filterHeader">
-        <h4>Filter</h4>
-        <button onClick={ this.closeFilterBox } className="close">
-          <img src={ Cross } alt="Close"/>
-        </button>
+        <div className="filterHeader">
+          <h4>Filter</h4>
+          <button onClick={ this.closeFilterBox } className="close">
+            <img src={ Cross } alt="Close"/>
+          </button>
+        </div>
+        <div className="filterBoxes">
+          <FilterOption
+            name="bbq"
+            addFilter={this.getFilterValue}
+          />
+          <FilterOption
+            name="swim"
+            addFilter={this.getFilterValue}
+          />
+          <FilterOption
+            name="gym"
+            addFilter={this.getFilterValue}
+          />
+          <FilterOption
+            name="booze"
+            addFilter={this.getFilterValue}
+          />
+          <FilterOption
+            name="bad"
+            addFilter={this.getFilterValue}
+          />
+          <FilterOption
+            name="bad"
+            addFilter={this.getFilterValue}
+          />
+          <FilterOption
+            name="bad"
+            addFilter={this.getFilterValue}
+          />
+          <FilterOption
+            name="bad"
+            addFilter={this.getFilterValue}
+          />
+          <FilterOption
+            name="bad"
+            addFilter={this.getFilterValue}
+          />
+        </div>
       </div>
-      <div className="filterBoxes">
-        <FilterOption
-          name="bbq"
-          addFilter={this.getFilterValue}
-        />
-        <FilterOption
-          name="swim"
-          addFilter={this.getFilterValue}
-        />
-        <FilterOption
-          name="gym"
-          addFilter={this.getFilterValue}
-        />
-        <FilterOption
-          name="booze"
-          addFilter={this.getFilterValue}
-        />
-        <FilterOption
-          name="bad"
-          addFilter={this.getFilterValue}
-        />
-        <FilterOption
-          name="bad"
-          addFilter={this.getFilterValue}
-        />
-        <FilterOption
-          name="bad"
-          addFilter={this.getFilterValue}
-        />
-        <FilterOption
-          name="bad"
-          addFilter={this.getFilterValue}
-        />
-        <FilterOption
-          name="bad"
-          addFilter={this.getFilterValue}
-        />
-      </div>
-    </div>
       <button onClick={ this.closeFilterBox } className="okSearch">
         Filter
       </button>
-      <div>
-      {
-        // this.state.choosenFilters.map(function(filter, index){
-        //   return <p key={index}> {filter} </p>;
-        // })
-        this.state.choosenFilters
-      }
-    </div>
-    </div>
 
-  )
-}
-}
+    </div>
+    </div>
+  )}}
 
 export default FilterBox;
